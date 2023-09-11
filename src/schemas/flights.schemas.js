075b6abdc84d207b5cac99b83499ba7e1dc5b6fa
GDMeira <gabrieldega970@gmail.com>
2023-09-11 19:08:06 -0300

    refac: adição da validação de número inteiro nos schemas que recebem ids.

--- src/schemas/flights.schemas.js
+++ src/schemas/flights.schemas.js
@@ -5,14 +5,14 @@ const Joi = JoiBase.extend(JoiDate); // extend Joi with Joi Date
 
 
 export const flightsSchema = Joi.object({
-    origin: Joi.number().min(1).required(),
-    destination: Joi.number().min(1).required(),
+    origin: Joi.number().integer().min(1).required(),
+    destination: Joi.number().integer().min(1).required(),
     date: Joi.date().format("DD-MM-YYYY").required()
 })
 
 export const travelSchema = Joi.object({
-    passengerId: Joi.number().min(1).required(),
-    flightId: Joi.number().min(1).required()
+    passengerId: Joi.number().integer().min(1).required(),
+    flightId: Joi.number().integer().min(1).required()
 })
 
 export const dateSchema = Joi.date().format("DD-MM-YYYY");
